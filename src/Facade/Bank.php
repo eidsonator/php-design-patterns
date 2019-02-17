5e802c2c216e32814f108da0043d8ff4d48827e6
todd <teidson@bapm.com>
2019-02-17 07:58:33 -0500

    Start bank

---
+++ src/Facade/Bank.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: todd
+ * Date: 2/17/19
+ * Time: 7:57 AM
+ */
+
+namespace App\Facade;
+
+
+class Bank
+{
+
+}
---
+++ tests/Facade/Bank.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: todd
+ * Date: 2/17/19
+ * Time: 7:57 AM
+ */
+
+namespace Tests\Facade;
+
+use PHPUnit\Framework\TestCase;
+
+class Bank extends TestCase
+{
+
+}
